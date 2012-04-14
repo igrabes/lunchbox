0cc14dbd933d34b35d4703111deeaa17af6e6b13
Ian <ian.grabill@gmail.com>
2012-04-14 16:31:37 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController  
+  
+  def index
+    
+  end
+  
+  def signup
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchbox::Application.config.secret_token = '3860264b4b55345d11bfeee7c1c332ce0cf72818972ea654e0d3162a86c8f9d374781310768ee02844b34f45cccc9bf36d86c669e022d1dad42d6b29299073c5'
